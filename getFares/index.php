c1a568e1f2d7a3bccfb540d5ba4b9d8ff305ea51
Ajay Vsns <vsns.2529@gmail.com>
2016-10-22 01:09:59 +0530

    First commit

---
+++ getFares/index.php
@@ -0,0 +1,27 @@
+<?php
+$header = array();
+$data='{  "FareSearchRequest": {    "Origin": "LAX",    "Destination": "SIN",    "DepartureDate": "2016-10-31",
+    "ReturnDate": "",
+    "PassengerType": "ADT"
+  }
+}';
+$header[] = 'Access-Control-Allow-Headers : *';
+$header[] = 'Content-type: application/json';
+$header[] = 'Authorization: Bearer jHBGzFs1LRennqdGsdFj_pgxw3ka';
+$url_link='http://apphonics.tcs.com/public/travelport/faresFinder/v1.0.0/getFaresList';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch, CURLOPT_URL, $url_link);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION,false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1');
+$html = curl_exec($ch);
+curl_close($ch);
+
+header('Content-type: application/json');
+echo json_encode(json_decode($html, true));
+
+?>
